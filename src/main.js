2bf85c9eed00f7f50c20bb4d02bfcf123c39cc6a
AnaRodea <a.rod3a@gmail.com>
2020-03-04 13:43:53 -0600

    credenciales

--- src/main.js
+++ src/main.js
@@ -4,37 +4,15 @@ import {
 } from './data.js';
 
 
-//  console.log(unknownStatus());
-//  console.log(episodeOne());
-// console.log(vampireCharacters());
-// console.log(animalCharacters());
-//  console.log(deadCharacters());
-//  console.log(unknownStatus());
-//  console.log(aliveCharacters());
-//  console.log(maleCharacters());
-//  console.log(genderlessCharacters());
-//  console.log(episodeOne());
-//Selectores con los siguientes valores:
-// gend="Female"
-// gend="Male"
-// gend="Genderless"
-// stat="Alive", "Dead", "unknown"
-//epi='https://rickandmortyapi.com/api/episode/1'
-// spec="Human", "Poopybutthole", "Superhuman"
-// "Robot", "Mytholog"
-// "Vampire"
-// "Animal"
-// "Cronenberg"
-// "Alien"
-// "Humanoid"
-//
 
 //  Nodos
 let gends = document.querySelectorAll('.gen');
 let specs = document.querySelectorAll(".spec");
 let stats = document.querySelectorAll(".stat");
+// let results = document.querySelector(".content");
 
-// // //Listeners
+
+//  Listeners con funciones anónimas
 gends.forEach(btn=>btn.addEventListener('click', e=>{
   console.log(getCharactersByGender(e.target.id))
 }
@@ -49,3 +27,9 @@ stats.forEach(btn=>btn.addEventListener('click', e=>{
   console.log(getCharactersByStatus(e.target.id))
 }
 ))
+
+// Ejemplo Bliss crear opcion dentro del selector
+// let results = document.createElement("item")
+// item.name="Name"
+// item.gender="Gender"
+// content.appendChild(item)
