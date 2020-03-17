fc22f286af4f3bf8316873085f3b3c49ea3d00c2
AniaCuapio <ania.cuapio@gmail.com>
2020-03-16 19:18:09 -0600

    Modifiqué el slider.

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ const specs = document.querySelectorAll('.spec');
 const stats = document.querySelectorAll('.stat');
 const epis = document.querySelectorAll(".epi");
 const credenciales = document.querySelector('#Credenciales');
+const slider = document.querySelector("#Slider");
 
 // Función callback para crear la tarjeta de cada personaje
 
@@ -16,24 +17,23 @@ function createTablePerCharacter(array) {
   credenciales.innerHTML = '';
   for (let item of array) {
   credenciales.innerHTML +=
-  `
-<div class="flip-card">
-<div class="flip-card-inner">
- <div class= "front">
+  `<div class="flip-card">
+    <div class="flip-card-inner">
+      <div class= "front">
         <img class= "" width=300px src="${item.image}" alt="">
-        <h2 class="" id="name">Name:"${item.name}" </h2> </div>
-        <div class="back">
-        <p id="name"> Name:"${item.name}"</p> 
-        <p id="species"> Especie:"${item.species}"</p>
-        <p id="gender">Género::"${item.gender}"</p>
-        <p id="status"> Status:"${item.status}"</p>
-        <p id="origin"> Origen:"${item.origin.name}" </p>
-        <p id="location"> Ubicación:"${item.location.name}" </p>
-        </div>
-</div>
-</div>
-  `
+        <h2 class="" id="name">${item.name} </h2> </div>
+          <div class="back">
+            <p id="name"> Name:${item.name}</p> 
+            <p id="species"> Especie:${item.species},${item.type}</p>
+            <p id="gender">Género:${item.gender}</p>
+            <p id="status"> Status:${item.status}</p>
+            <p id="origin"> Origen:${item.origin.name} </p>
+            <p id="location"> Ubicación:${item.location.name} </p>
+          </div>
+      </div>
+  </div>`
 }
+slider.style = "display:none";
 }
 //TO DO => <p id="episode"> Episodios:"${item.episode}"</p>
 
